5dc2811bd2f31a8e08c2434ec63be4c6331af681
Jean Damaceno <jeandamaceno3@gmail.com>
2024-04-29 10:19:42 -0300

    Ajuste sobre less

--- WhatsApp/Button/Helper/Data.php
+++ WhatsApp/Button/Helper/Data.php
@@ -10,35 +10,29 @@ class Data extends \Magento\Framework\App\Helper\AbstractHelper
     ) {
         parent::__construct($context);
     }
-
 	/**
      * Return status
      *
      * @return string
      */
-
 	public function getStatus(){
 		return $this->scopeConfig->getValue('whatsapp/whatsapp_button/enable', 
                         \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
 	}
-
     /**
      * Return phone
      *
      * @return string
      */
-
     public function getPhone(){
         return $this->scopeConfig->getValue('whatsapp/whatsapp_button/phone_number', 
                         \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
     }
-
     /**
      * Return message
      *
      * @return string
      */
-
     public function getMessage(){
         return $this->scopeConfig->getValue('whatsapp/whatsapp_button/message', 
                         \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
